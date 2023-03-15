069df5ac2ddfc8140158a7b43ba13530de66c4fa
kritikmodi <kritikmodi@gmail.com>
2023-03-16 02:07:14 +0530

    I'm actually 22

--- src/variables.rs
+++ src/variables.rs
@@ -11,7 +11,7 @@ fn variables() {
     //Varibles are defined as <variable_scope> <variable_name> : <data_type> = <value>
     const ONE_MIL : u32 = 1_000_000;
     const PI : f32 = 3.141592;
-    let age : &str = "47";
+    let age : &str = "21";
     let mut age : u32 = age.trim().parse().expect("Age wasn't converted!");
 
     age = age + 1;
