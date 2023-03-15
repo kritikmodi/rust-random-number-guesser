5f6e4b84a90af81af228f2c4ba5f136c30830647
kritikmodi <kritikmodi@gmail.com>
2023-03-16 02:09:08 +0530

    Add comments

--- src/variables.rs
+++ src/variables.rs
@@ -8,9 +8,12 @@ use std::cmp::Ordering;
 
 fn variables() {
 
-    //Varibles are defined as <variable_scope> <variable_name> : <data_type> = <value>
+    //Varibles are defined as <variable_scope> <variable_name> : <datatype> = <value>
     const ONE_MIL : u32 = 1_000_000;
     const PI : f32 = 3.141592;
+
+    //there can be multiple variables with the same name but different datatypes
+    //this is known as "shadowing"
     let age : &str = "21";
     let mut age : u32 = age.trim().parse().expect("Age wasn't converted!");
 
